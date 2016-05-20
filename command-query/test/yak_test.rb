8d5fcd7b751fe2f6d3b43f30678df59c2b89d2f3
saylerb <sayler.b@gmail.com>
2016-05-19 23:09:56 -0600

    yak complete

--- command-query/test/yak_test.rb
+++ command-query/test/yak_test.rb
@@ -1,7 +1,7 @@
 gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative 'yak'
+require_relative '../lib/yak'
 
 class YakTest < Minitest::Test
   def test_yak_is_hairy
@@ -10,10 +10,8 @@ class YakTest < Minitest::Test
   end
 
   def test_shave_the_yak
-    skip
     yak = Yak.new
     yak.shave
     refute yak.hairy?, "The shaven yak is no longer hairy."
   end
-
 end
