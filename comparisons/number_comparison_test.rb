8dd0431c2d6be710694db6ab94f8b9b4c752c37b
saylerb <sayler.b@gmail.com>
2016-06-21 15:07:18 -0600

    complete number comparison test

--- comparisons/number_comparison_test.rb
+++ comparisons/number_comparison_test.rb
@@ -9,47 +9,47 @@ class NumberComparisonsTest < Minitest::Homework
   def test_two_ints
     n1 = 1
     n2 = 1
-    maybe n1.object_id == n2.object_id
-    maybe n1 == n2
-    maybe n1 <= n2
-    maybe n1 >= n2
-    assert_equal __, n1 <=> n2
+    assert n1.object_id == n2.object_id
+    assert n1 == n2
+    assert n1 <= n2
+    assert n1 >= n2
+    assert_equal 0, n1 <=> n2
   end
 
   def test_float_and_int
     n1 = 1
     n2 = 1.0
-    maybe n1.object_id == n2.object_id
-    maybe n1 == n2
-    maybe n1 <= n2
-    maybe n1 >= n2
-    assert_equal __, n1 <=> n2
-    assert_equal __, n2 <=> n1
+    refute n1.object_id == n2.object_id
+    assert n1 == n2
+    assert n1 <= n2
+    assert n1 >= n2
+    assert_equal 0, n1 <=> n2
+    assert_equal 0, n2 <=> n1
   end
 
   def test_two_different_ints
     n1 = 1
     n2 = 2
-    maybe n1.object_id == n2.object_id
-    maybe n1 < n2
-    maybe n1 > n2
-    maybe n1 <= n2
-    maybe n1 >= n2
-    assert_equal __, n1 <=> n2
-    assert_equal __, n2 <=> n1
+    refute n1.object_id == n2.object_id
+    assert n1 < n2
+    refute n1 > n2
+    assert n1 <= n2
+    refute n1 >= n2
+    assert_equal -1, n1 <=> n2
+    assert_equal 1, n2 <=> n1
   end
 
   def test_between
     one = 1
     five = 5
     ten = 10
-    maybe five.between?(one, ten)
-    maybe five.between?(ten, one)
-    maybe one.between?(five, ten)
+    assert five.between?(one, ten)
+    refute five.between?(ten, one)
+    refute one.between?(five, ten)
   end
 
   def test_sort
     numbers = [9.99, 10.15, 2.75, 1.01]
-    assert_equal __, numbers.sort
+    assert_equal [1.01, 2.75, 9.99, 10.15], numbers.sort
   end
 end
